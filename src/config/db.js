24ea10948d63d053b64370bdae3beee48f1686da
Siddhu2008 <sawhuman2008@gmail.com>
2026-03-19 02:14:38 +0530

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
+// src/config/db.js
 const connectDB = async () => {
   const uri = process.env.MONGO_URI;
-  if (!uri) {
-    throw new Error("MONGO_URI is not set in environment variables");
+  if (!uri) throw new Error("MONGO_URI is not set");
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Mongo connect failed:", err.message);
+    throw err;
   }
-  await mongoose.connect(uri);
-  console.log("MongoDB connected");
 };
 
 export default connectDB;
